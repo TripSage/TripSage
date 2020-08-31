12a55247c586586692424574edfca646621f5de8
Dhruvil <dhruvilsshah@gmail.com>
2020-08-31 16:26:56 -0400

    Revert "update installed Apps with Form app"
    This reverts commit 77945796

--- TripSage/settings.py
+++ TripSage/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Form.apps.FormConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
