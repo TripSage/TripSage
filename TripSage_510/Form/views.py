318bc4460e8b316667dcfdf289d58b9af87a6ca4
Poorvaja Penmetsa <ppenmet@ncsu.edu>
2020-09-01 14:40:36 -0700

    Update Form app

--- TripSage_510/Form/views.py
+++ TripSage_510/Form/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Trip, Dest, Bud
 
 # Create your views here.
-def index(request):
+def index(request, trips_id):
     trips = Trip.objects.all()
+    trip = get_object_or_404(Trip, pk = trips_id)
+    #context = {"trips":  trips}
+    return render(request, 'Form/index.html',{"trips":  trip})
+
+def display(request, trips_id):
+
+    trips = get_object_or_404(Trip, pk = trips_id)
+    return render(request, 'Form/display.html', {"trips":  trips})
