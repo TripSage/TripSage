a18ac9ff551cce27e8cb07750f604de51148f7e3
Sameer Adhikari <sadhika2@ncsu.edu>
2020-09-17 18:01:00 -0400

    Fixed ajax call issue
    Fixed ajax call issue, got the data in response function

--- tripHome/views.py
+++ tripHome/views.py
@@ -8,7 +8,9 @@ import xml.etree.ElementTree as ET
 import requests
 import json
 
+@csrf_exempt
 def getResponse(request):
+    clientData = json.loads(request.POST['requestData'])
     response = requests.get("https://maps.googleapis.com/maps/api/place/textsearch/xml?query=restaurants+in+Raleigh"
                             "&key=AIzaSyAIsboWfXVchmgBxPGKG5lUF9AENUKcSI8")
     root = ET.fromstring(response.content)
@@ -36,37 +38,6 @@ def getResponse(request):
 
     new_data = zip(items_name, items_rating)
     return render(request, 'result.html', {'data': new_data})
-
-@csrf_exempt
-def postData(request):
-    if request.is_ajax():
-        #do something
-        tripInstance = Trip()
-        form = TripForm(request.POST or None)
-        if form.is_valid():
-            tripInstance.transp = request.POST['transp']
-            # iterating over choice in html:
-            # {% for value, text in form.[field_name].field.choices %}
-            # {{ value }}: {{ text }}
-            # % endfor %}
-            tripInstance.no_people = request.POST['no_people']
-            tripInstance.start = request.POST['start_date']
-            tripInstance.end = request.POST['end_date']
-            tripInstance.source_city = request.POST['source_city']
-
-        #request_data = request.POST
-        return HttpResponse("OK")
-
-        # try:
-        #     transp = request.POST['transp']
-        #     no_people = request.POST['no_people']
-        #     start = request.POST['start_date']
-        #     end = request.POST['end_date']
-        #     source_city = request.POST['soource_city']
-
-        # except (KeyError, Trip.DoesNotExist):
-        # #return render(request)
-        #     return None
 
 
 def index(request):
