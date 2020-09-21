e508bfc38ae6d6c4950beb442fdc973e30909177
Sameer Adhikari <saadhika@adobe.com>
2020-09-21 18:22:46 -0400

    Fixed oylint issue for large number of variables in a function

--- tripHome/views.py
+++ tripHome/views.py
@@ -9,30 +9,27 @@ from django.views.decorators.csrf import csrf_exempt
 import requests
 import xmltodict
 
+# Map for the type of the trip to the places user can visit
+TYPES_PLACE_MAP = {
+    "adventurous": ["tourist_attraction", "stadium"],
+    "kids": ["amusement_park", "museum"],
+    "relaxing": ["art_gallery", "church", "spa"],
+}
+
 @csrf_exempt
 def get_response(request):
     """
     Function to get data from api When user click on search
     a post call is made which lands on this function
     """
-
     # getting the landing page data in form of dictionary
     client_data = json.loads(request.POST["requestData"])
-    # getting the list of all the destinations
-    location_list = client_data["destination_selected"]
-    trip_kind = client_data["tripType"]  # getting trip type
 
-    # Map for the type of the trip to the places user can visit
-    type_places_map = {
-        "adventurous": ["tourist_attraction", "stadium"],
-        "kids": ["amusement_park", "museum"],
-        "relaxing": ["art_gallery", "church", "spa"],
-    }
     complete_data = set()
     final_data = {}
-    for city in location_list:
-        for types in trip_kind:
-            for place in type_places_map[types]:
+    for city in client_data["destination_selected"]:
+        for types in client_data["tripType"]:
+            for place in TYPES_PLACE_MAP[types]:
                 api = (
                     "https://maps.googleapis.com/maps/api/" +
                     "place/textsearch/xml?query="
@@ -41,26 +38,19 @@ def get_response(request):
                     + city
                     + "&key=AIzaSyAIsboWfXVchmgBxPGKG5lUF9AENUKcSI8"
                 )
-                response = requests.get(api)
-                data_dict = xmltodict.parse(response.content)
-                json_data = json.dumps(data_dict)
-                results = json.loads(json_data)
-                values = results.items()
+                data_dict = xmltodict.parse(requests.get(api).content)
+                results = json.loads(json.dumps(data_dict))
                 list_items = []
-                for item in values:
+                for item in results.items():
                     list_items = item
-                selected_list = list_items[1].items()
                 val = []
-                for vals in selected_list:
-                    val.append(vals)
+                for values in list_items[1].items():
+                    val.append(values)
                 if len(val) > 1:
-                    data = val[1][1]
                     complete_data = {}
-                    for item in data[:6]:
-                        temp_data = json.dumps(item)
-                        loaded_r = json.loads(temp_data)
-                        name = str(loaded_r["name"])
-                        complete_data[name] = str(loaded_r["rating"])
+                    for values in val[1][1][:6]:
+                        loaded_r = json.loads(json.dumps(values))
+                        complete_data[str(loaded_r["name"])] = str(loaded_r["rating"])
         final_data[city] = complete_data
     return HttpResponse(json.dumps(final_data))
 
