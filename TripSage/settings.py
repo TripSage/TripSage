ddec940dd971cfa1664404e691bf47cd750995d6
santoshpavan <santoshpavan.psp@gmail.com>
2020-10-08 16:21:56 -0400

    basic user login, logout, registration implemeplemented- frontend

--- TripSage/settings.py
+++ TripSage/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tripHome',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -55,8 +56,7 @@ ROOT_URLCONF = 'TripSage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# User authentication paths
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
