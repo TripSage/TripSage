d93c7f4fbabe99f84aa09af0f2fbda02ac98e9db
akashsrikanth <akashsrikanth14@gmail.com>
2020-10-23 01:02:42 -0500

    removing unused function

--- tripHome/views.py
+++ tripHome/views.py
@@ -33,33 +33,34 @@ def get_response(request):
     # getting the landing page data in form of dictionary
     client_data = json.loads(request.POST["requestData"])
     final_data = {}
-    
-    for city in client_data["destination_selected"]:
-        complete_data = {}
-        for types in client_data["tripType"]:
-            for place in TYPES_PLACE_MAP[types]:
-                api = (
-                    "https://maps.googleapis.com/maps/api/" +
-                    "place/textsearch/xml?query="
-                    + place
-                    + "+in+"
-                    + city
-                    + "&key=AIzaSyAIsboWfXVchmgBxPGKG5lUF9AENUKcSI8"
-                )
-                data_dict = xmltodict.parse(requests.get(api).content)
-                results = json.loads(json.dumps(data_dict))
-                list_items = []
-                for item in results.items():
-                    list_items = item
-                val = []
-                for values in list_items[1].items():
-                    val.append(values)
-                if len(val) > 1:
-                    for values in val[1][1][:3]:
-                        loaded_r = json.loads(json.dumps(values))
-                        complete_data[str(loaded_r["name"])] = str(loaded_r["rating"])
-        final_data[city] = complete_data
-    return HttpResponse(json.dumps(final_data))
+    client_data = 1
+    final_data = 1
+#     for city in client_data["destination_selected"]:
+#         complete_data = {}
+#         for types in client_data["tripType"]:
+#             for place in TYPES_PLACE_MAP[types]:
+#                 api = (
+#                     "https://maps.googleapis.com/maps/api/" +
+#                     "place/textsearch/xml?query="
+#                     + place
+#                     + "+in+"
+#                     + city
+#                     + "&key=AIzaSyAIsboWfXVchmgBxPGKG5lUF9AENUKcSI8"
+#                 )
+#                 data_dict = xmltodict.parse(requests.get(api).content)
+#                 results = json.loads(json.dumps(data_dict))
+#                 list_items = []
+#                 for item in results.items():
+#                     list_items = item
+#                 val = []
+#                 for values in list_items[1].items():
+#                     val.append(values)
+#                 if len(val) > 1:
+#                     for values in val[1][1][:3]:
+#                         loaded_r = json.loads(json.dumps(values))
+#                         complete_data[str(loaded_r["name"])] = str(loaded_r["rating"])
+#         final_data[city] = complete_data
+#     return HttpResponse(json.dumps(final_data))
 
 def results_page(request):
     """
